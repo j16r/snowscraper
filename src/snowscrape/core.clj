f8ecaf723313abce9f68616a411cfb34cde4556c
John Barker <jebarker@gmail.com>
2012-10-16 19:59:46 -0400

    New snowscrape lein project

---
+++ src/snowscrape/core.clj
@@ -0,0 +1,1 @@
+(ns snowscrape.core)
---
+++ test/snowscrape/test/core.clj
@@ -0,0 +1,6 @@
+(ns snowscrape.test.core
+  (:use [snowscrape.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
